e2cbead71328dcd07234f0d176ca5c1d062763f2
Barbara Bateli <barbarabateli@MacBook-Air-de-Barbara.local>
2021-06-21 01:31:30 -0300

    done

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor(items, itemsNum) {
+    this.items = [];
+    this.length = 0;
 
-  add(item) {}
+    list.add(5);
+    list.add(1);
+    list.add(3);
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length) throw new Error("OutOfBounds");
+    return this.items[pos - 1];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items[this.length - 1];
+    } else throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0];
+    } else throw new Error("EmptySortedList");
+  }
 
-  avg() {}
-}
+  sum() {
+    return this.items.reduce((item, reducer) => {
+      return item + reducer;
+    }, 0);
+  }
+
+  avg() {
+    if (this.length) {
+      return this.sum() / this.length;
+    } else throw new Error("EmptySortedList");
+  }
+};
+module.exports = SortedList;
